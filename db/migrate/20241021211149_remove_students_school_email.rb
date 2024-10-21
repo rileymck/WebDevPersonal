0651090a222128d2b94b49fd92ca3311835167f2
Riley McKenzie <rmckenz4@msudenver.edu>
2024-10-21 15:34:30 -0600

    removed school_email

---
+++ db/migrate/20241021211149_remove_students_school_email.rb
@@ -0,0 +1,4 @@
+class RemoveStudentsSchoolEmail < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
---
+++ db/migrate/20241021211543_remove_school_email_from_students.rb
@@ -0,0 +1,5 @@
+class RemoveSchoolEmailFromStudents < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :students, :school_email, :string
+  end
+end
